704cbcd2d1e2968c3ff03e34aad9d690104886a8
Minh Vu Nguyen <m.nguyen@devduck.de>
2024-05-15 00:36:05 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        // These are all the locales you want to support in
+        // your application
+        locales: ['en', 'de'],
+        // This is the default locale you want to be used when visiting
+        // a non-locale prefixed path e.g. `/hello`
+        defaultLocale: 'de',
+        localeDetection: true
+      },
+};
 
 export default nextConfig;
